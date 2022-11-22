1602d10b7190a4984cd3ede3c4d5369ff1562a89
ThejeshwarAB <abibastheja@gmail.com>
2022-11-22 13:10:59 +0530

    Added changes in UI

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const form1 = document.querySelector('.form1');
-const form2 = document.querySelector('.form2');
+const form = document.querySelector('.form');
 const title = document.querySelector('.title');
 
 var json = {};
@@ -11,14 +10,15 @@ async function getData(url) {
         .then(result => result.json())
 }
 
-form1.addEventListener('submit', async e => {
+// form1.addEventListener('submit', async e => {
+    window.onload = async () => {
 
     // prevents page reload on submit
-    e.preventDefault();
+    // e.preventDefault();
 
     // hides pref form, displays qna form
-    form1.classList.add('d-none');
-    form2.classList.remove('d-none');
+    // form1.classList.add('d-none');
+    // form2.classList.remove('d-none');
 
     // gets json data
     await getData('./qna.json');
@@ -28,20 +28,47 @@ form1.addEventListener('submit', async e => {
     qna = json['qna'];
 
     var templateString = '';
+    var mainIndex = 0;
 
     qna.forEach(element => {
-        (element["answerOptions"].forEach(answerOption=>console.log(answerOption)))
+
+        var tempString = '';
+        var tempArray = element["answerOptions"];
+        var index = 0;
+        var isChecked = '';
+
+        while (index !== 3) {
+
+            // console.log("Called")
+
+            var randomNo = Math.floor(Math.random() * (tempArray.length - 1));
+
+            isChecked = index === 0 ? 'checked' : ''
+
+            tempString += `<div class='form-check'>
+                                <input class="form-check-input" type="radio" name="${mainIndex}" 
+                                    id="${tempArray[randomNo]}" ${isChecked}>
+                                <label class="form-check-label" for="${tempArray[randomNo]}">
+                                    ${tempArray[randomNo]}
+                                </label>
+                            </div>`
+
+            tempArray = tempArray.filter(item => item !== tempArray[randomNo])
+
+            index++;
+        }
+
+        mainIndex++;
+
+        // console.log(tempString)
+
         templateString +=
             `
         <div class='card p-3 m-3 col-6'>
             <p>${element["question"]}</p>
             <div class="form-check">
-            ${element["answerOptions"].forEach(answerOption => {
-                `<input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                    <label class="form-check-label" for="flexRadioDefault1">
-                        ${answerOption}
-                    </label>`
-            })}
+
+                ${tempString}
 
             </div>
         </div>
@@ -49,7 +76,7 @@ form1.addEventListener('submit', async e => {
         // console.log(element)
     });
 
-    form2.innerHTML = templateString;
+    form.innerHTML = templateString + form.innerHTML;
 
 
-})
+}
