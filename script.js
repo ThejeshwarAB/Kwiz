94c59fec3f39f1e9c2a52ae1c5dd87b67a962724
ThejeshwarAB <abibastheja@gmail.com>
2022-11-22 20:00:15 +0530

    Added some pretty printing

--- script.js
+++ script.js
@@ -10,73 +10,76 @@ async function getData(url) {
         .then(result => result.json())
 }
 
-// form1.addEventListener('submit', async e => {
-    window.onload = async () => {
+// Durstenfeld's shuffle algorithm
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
 
-    // prevents page reload on submit
-    // e.preventDefault();
-
-    // hides pref form, displays qna form
-    // form1.classList.add('d-none');
-    // form2.classList.remove('d-none');
-
+window.onload = async () => {
     // gets json data
     await getData('./qna.json');
-    console.log(json);
-
-
     qna = json['qna'];
 
+    // tracking variables
     var templateString = '';
-    var mainIndex = 0;
+    var mainIndex = 0; // tracks the no. of questions
 
+    shuffleArray(qna);
+    console.log(qna)
+    // iterating through each elements
     qna.forEach(element => {
 
+        //  temporary variables
         var tempString = '';
         var tempArray = element["answerOptions"];
-        var index = 0;
+        var tempNumber = element["questionNumber"];
+        var index = 0; //  tracks the no. of answer options
         var isChecked = '';
 
+        // randomize the options
         while (index !== 3) {
+            var randomNo = Math.floor(Math.random() * (tempArray.length));
 
-            // console.log("Called")
-
-            var randomNo = Math.floor(Math.random() * (tempArray.length - 1));
-
+            // checks the first option by default
             isChecked = index === 0 ? 'checked' : ''
 
             tempString += `<div class='form-check'>
-                                <input class="form-check-input" type="radio" name="${mainIndex}" 
-                                    id="${tempArray[randomNo]}" ${isChecked}>
+                                <input class="form-check-input" type="radio" name="q${mainIndex}${tempNumber}" 
+                                    id="${tempNumber}" value="${tempArray[randomNo]}" ${isChecked}>
                                 <label class="form-check-label" for="${tempArray[randomNo]}">
                                     ${tempArray[randomNo]}
                                 </label>
                             </div>`
 
             tempArray = tempArray.filter(item => item !== tempArray[randomNo])
-
             index++;
         }
-
         mainIndex++;
 
-        // console.log(tempString)
-
         templateString +=
-            `
-        <div class='card p-3 m-3 col-6'>
-            <p>${element["question"]}</p>
-            <div class="form-check">
-
+            `<div class='card p-3 m-3 col-6'>
+                <p>${element["question"]}</p>
+                <div class="form-check">
                 ${tempString}
-
-            </div>
-        </div>
-        `
-        // console.log(element)
+                </div>
+            </div>`
     });
-
+    // updates the form with questions on
     form.innerHTML = templateString + form.innerHTML;
-
-
 }
+
+form.addEventListener('submit', e => {
+    e.preventDefault(); // prevents the page from reloading
+    console.log(form)
+    for (var index = 0; index < 3; index++) {
+        var tempName = 'q' + index.toString();
+        console.log(index)
+        console.log(form[tempName].value);
+        // if(form[tempName].value===)
+    }
+})
